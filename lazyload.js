4bffabd2edff0ee51467060b1a794d19b7aad588
欧吉增 <oujizeng@oujizengdeMacBook-Pro.local>
2018-06-02 13:59:43 +0800

    v1.0.0

---
+++ lazyload.js
@@ -0,0 +1,83 @@
+/*
+* author: "oujizeng",
+* license: "MIT",
+* name: "lazyload.js",
+* version: "1.0.0"
+*/
+
+(function (root, factory) {
+    if (typeof module != 'undefined' && module.exports) {
+        module.exports = factory();
+    } else {
+        root['lazyload'] = factory();
+    }
+}(this, function () {
+    'use strict'
+
+    var utils = {
+        // 节流函数
+        throttle: function (method, delay, duration) {
+            var timer = null;
+            var begin = new Date();
+            return function () {
+                var context = this,
+                    args = arguments;
+                var now = new Date();
+                clearTimeout(timer);
+                if(now - begin >= duration) {
+                    method.apply(context, args);
+                    begin = now;
+                } else {
+                    timer = setTimeout(function() {
+                        method.apply(context, args);
+                    }, delay);
+                }
+            }
+        }
+    }
+
+    var lazyload = {
+
+        init: function() {
+            var win = window,
+                doc = document,
+                clientHeight = win.innerHeight || doc.documentElement.clientHeight || win.screen.availHeight;
+
+            // 获取懒加载图片
+            var imgs = doc.querySelectorAll('img.lazyload');
+            // 桥接，方便销毁
+            var detect = utils.throttle(lazyLoad, 0, 1000/60);
+            // 执行一次
+            detect();
+
+            // 绑定 scroll 和 resize
+            win.addEventListener('scroll', detect, false);
+            win.addEventListener('resize', detect, false);
+
+            // 懒加载方法
+            function lazyLoad () {
+                [].forEach.call(imgs, function (img) {
+                    var src = img.getAttribute('data-src');
+                    if (src) {
+                        var bound = img.getBoundingClientRect();
+                        if (bound.top < clientHeight + 200) {
+                            img.setAttribute('src', src);
+                            img.removeAttribute('data-src');
+                            // fadeIn显示
+                            img.addEventListener('load' , function() {
+                                img.classList.add('lazyloaded');
+                            }, false);
+                        }
+                    }
+                });
+                //完成所有替换后注销事件
+                [].every.call(imgs, function (img) {
+                    return !img.getAttribute('data-src')
+                }) && (win.removeEventListener("scroll", detect, false),
+                    win.removeEventListener("resize", detect, false));
+            }
+        }
+    }
+
+    return lazyload;
+}));
