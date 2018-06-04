f8517fe810a15cc3f6103cd450a1c4c62205ca0c
欧吉增 <oujizeng@oujizengdeMacBook-Pro.local>
2018-06-04 10:26:35 +0800

    处理2倍以上屏加载高清图

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # img-lazyload
-img lazyload
+移动端图片懒加载的基本实现，可以在此基础上丰富。
+
+## 实现技术
+> * 原生js实现，无依赖；
+
+## 特点
+> * 代码非常小，只有几十行，简单易懂，方便进行扩展和修改；
+> * 无依赖，这样就可以用于任何地方了；
+> * 支持根据用户端devicePixelRatio加载不同尺寸的图片；
+
+## 待做功能
+> * 向上滚动懒加载；
+> * 增加一些回调钩子；
+
+## 预览
+> * 页面[点击这里](https://yangyuji.github.io/img-lazyload/demo.html)
+> * ![扫描二维码](https://github.com/yangyuji/img-lazyload/blob/master/qrcode.png)
--- lazyload.js
+++ lazyload.js
@@ -2,7 +2,7 @@
 * author: "oujizeng",
 * license: "MIT",
 * name: "lazyload.js",
-* version: "1.0.0"
+* version: "1.1.0"
 */
 
 (function (root, factory) {
@@ -41,7 +41,11 @@
         init: function() {
             var win = window,
                 doc = document,
+                dpr = win.devicePixelRatio || 1,
                 clientHeight = win.innerHeight || doc.documentElement.clientHeight || win.screen.availHeight;
+
+            //被iframe引用时，禁止缩放
+            dpr = win.top === win.self ? dpr : 1;
 
             // 获取懒加载图片
             var imgs = doc.querySelectorAll('img.lazyload');
@@ -57,12 +61,17 @@
             // 懒加载方法
             function lazyLoad () {
                 [].forEach.call(imgs, function (img) {
-                    var src = img.getAttribute('data-src');
+                    if (dpr >= 2) {
+                        var src = img.getAttribute('data-2x');
+                    } else {
+                        var src = img.getAttribute('data-src');
+                    }
                     if (src) {
                         var bound = img.getBoundingClientRect();
-                        if (bound.top < clientHeight + 200) {
+                        if (bound.top < clientHeight * 1.2) {
                             img.setAttribute('src', src);
                             img.removeAttribute('data-src');
+                            img.removeAttribute('data-2x');
                             // fadeIn显示
                             img.addEventListener('load' , function() {
                                 img.classList.add('lazyloaded');
