29df6c55d23b5cada7a1a56f0fc82715f3fb3a5a
yangyuji <394712273@qq.com>
2018-08-14 16:07:34 +0800

    update

--- README.md
+++ README.md
@@ -1,23 +1,18 @@
 # img-lazyload
-移动端图片懒加载的基本实现，可以在此基础上丰富。
+a light &amp; tiny image lazyload script with no dependancy
 
-## 实现技术
-> * 原生js实现，无依赖；
+## todo
+> * load different image according to different retina screen
+> * add the image loaded callback
 
-## 特点
-> * 代码非常小，只有几十行，简单易懂，方便进行扩展和修改；
-> * 无依赖，这样就可以用于任何地方了；
-> * 支持根据用户端devicePixelRatio加载不同尺寸的图片；
-
-## 待做功能
-> * 向上滚动懒加载；
-> * 增加一些回调钩子；
-
-## 使用方法
+## use
 ```javascript
-<img class="lazyload" data-src="./w375.jpg" data-2x="./w750.jpg" alt="">
+<img class="lazyload" data-src="./w375.jpg" alt="">
 ...
-document.addEventListener('DOMContentLoaded', function(e) {
-   lazyload.init();
+window.addEventListener('load', function(e) {
+   lazyload({
+       lazyClass: '.lazyload',
+       lazyAttr: 'data-src'
+   });
 }, false);
 ```
--- lazyload.js
+++ lazyload.js
@@ -3,91 +3,81 @@
 * license: "MIT",
 * github: "https://github.com/yangyuji/img-lazyload",
 * name: "lazyload.js",
-* version: "1.1.2"
+* version: "1.3.0"
 */
 
 (function (root, factory) {
     if (typeof module != 'undefined' && module.exports) {
         module.exports = factory();
     } else if (typeof define == 'function' && define.amd) {
-        define( function () { return factory(); } );
+        define(function () {
+            return factory();
+        });
     } else {
         root['lazyload'] = factory();
     }
 }(this, function () {
     'use strict'
 
-    var utils = {
-        // 节流函数
-        throttle: function (method, delay, duration) {
-            var timer = null;
-            var begin = new Date();
-            return function () {
-                var context = this,
-                    args = arguments;
-                var now = new Date();
-                clearTimeout(timer);
-                if(now - begin >= duration) {
-                    method.apply(context, args);
-                    begin = now;
-                } else {
-                    timer = setTimeout(function() {
-                        method.apply(context, args);
-                    }, delay);
+    function lazyload(opts) {
+        var win = window,
+            doc = document,
+            imgs = doc.querySelectorAll(opts.lazyClass);
+
+        if (imgs.length <= 0 || !opts.lazyAttr) return;
+
+        // throttle scroll and resize
+        win.addEventListener('scroll', scroller, false);
+        win.addEventListener('resize', scroller, false);
+        scroller();
+
+        function scroller() {
+            requestAnimationFrame(inView);
+        }
+
+        function inView() {
+            var wT = win.pageYOffset,
+                wB = wT + win.innerHeight,
+                hasLazy = false,
+                cRect, pT, pB, i = 0;
+
+            while (i < imgs.length) {
+                cRect = imgs[i].getBoundingClientRect();
+                pT = wT + cRect.top;
+                pB = pT + cRect.height;
+
+                if (imgs[i].hasAttribute(opts.lazyAttr)) {
+                    hasLazy = true;
+
+                    if (wT < pB && wB > pT) {
+                        loadImg(imgs[i]);
+                    }
                 }
+                i++;
+            }
+
+            if (!hasLazy) {
+                destroy();
             }
         }
-    }
 
-    var lazyload = {
-
-        init: function() {
-            var win = window,
-                doc = document,
-                dpr = win.devicePixelRatio || 1,
-                clientHeight = win.innerHeight || doc.documentElement.clientHeight || win.screen.availHeight;
-
-            //被iframe引用时，禁止缩放
-            dpr = win.top === win.self ? dpr : 1;
-
-            // 获取懒加载图片
-            var imgs = doc.querySelectorAll('img.lazyload');
-            // 执行一次
-            lazyLoad();
-
-            // 绑定 scroll 和 resize
-            win.addEventListener('scroll', lazyLoad, false);
-            win.addEventListener('resize', lazyLoad, false);
-
-            // 懒加载方法
-            function lazyLoad () {
-                utils.throttle(function () {
-                    [].forEach.call(imgs, function (img) {
-                        if (dpr >= 2) {
-                            var src = img.getAttribute('data-2x');
-                        } else {
-                            var src = img.getAttribute('data-src');
-                        }
-                        if (src) {
-                            var bound = img.getBoundingClientRect();
-                            if (bound.top < clientHeight * 1.2) {
-                                img.setAttribute('src', src);
-                                img.removeAttribute('data-src');
-                                img.removeAttribute('data-2x');
-                                // fadeIn显示
-                                img.addEventListener('load' , function() {
-                                    img.classList.add('lazyloaded');
-                                }, false);
-                            }
-                        }
-                    });
-                    //完成所有替换后注销事件
-                    [].every.call(imgs, function (img) {
-                        return !img.getAttribute('data-src')
-                    }) && (win.removeEventListener("scroll", lazyLoad, false),
-                        win.removeEventListener("resize", lazyLoad, false));
-                }, 0, 1000/60)
+        function loadImg(img) {
+            var src = img.getAttribute(opts.lazyAttr);
+            if (src) {
+                img.setAttribute('src', src);
+                img.removeAttribute(opts.lazyAttr);
+                // fadeIn display
+                img.addEventListener('load', function () {
+                    img.classList.add('lazyloaded');
+                }, false);
             }
+        }
+
+        function destroy() {
+            // remove scroll and resize listener
+            win.removeEventListener("scroll", scroller, false);
+            win.removeEventListener("resize", scroller, false);
+            imgs = [];
         }
     }
 
